fcbd16bd41a574a451f2f1a1ee7963b929f6e66f
ravis <ravissavoy49@gmail.com>
2021-09-22 10:50:44 -0500

    Added Sell items

--- Shop.cs
+++ Shop.cs
@@ -1,10 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace ShopRPG
 {
     class Shop
     {
+        private int _gold;
+        private Item[] _inventory;
+
+
+
+        public Shop(Item[] items)
+        {
+            _gold = 1000;
+
+            _inventory = items;
+        }
+
+        public bool Sell(Player player, int itemIndex, int playerIndex)
+        {
+            Item itemYouWant = _inventory[itemIndex];
+
+            if (player.Buy(itemYouWant, playerIndex))
+            {
+                _gold += itemYouWant.Cost;
+                return true;
+            }
+            return false;
+        }
+        public string[] GetItemNames()
+        {
+            string[] itemNames = new string[_inventory.Length];
+
+            for (int i = 0; i < _inventory.Length; i++)
+            {
+                itemNames[i] = _inventory[i].Name;
+            }
+
+            return itemNames;
+        }
+
+        public bool Load(StreamReader reader)
+        {
+            if (!int.TryParse(reader.ReadLine(), out _gold))
+                return false;
+
+            return true;
+        }
     }
 }
